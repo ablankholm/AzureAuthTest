c2fa1288d1cfd21717a829aebc8b456665c559f4
ablankholm <bo.bojsen@gmail.com>
2016-12-07 16:25:10 +0000

    aspnet.identity namespace removed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNet.Identity;
 
 namespace WebApplication.Controllers
 {
